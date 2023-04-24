0fd11508778d95e4ab3126f3e40cc87373bab9f4
juan triventi <jctriventi@ikeasistencia.com.ar>
2023-04-24 15:22:19 -0300

    solo guarda valores q no son nulos

--- index.js
+++ index.js
@@ -8,22 +8,20 @@ axios.get(url)
     const $ = cheerio.load(html);
     const cryptos = [];
     $('tbody tr').each((i, el) => {
-      const crypto = {};
-      crypto.name = $(el).find('td:nth-child(3) > div > a > div > div > p').text().trim();
-      crypto.price = $(el).find('td:nth-child(4) > div > a:first-child').text().trim();
-      const change_1h_icon = $(el).find('td:nth-child(5) > span > span');
-      crypto.change_1h_icon = change_1h_icon && change_1h_icon.attr('class') && change_1h_icon.attr('class').indexOf('up') !== -1 ? '✅' : '❗'; // Verifica si el elemento existe
-      crypto.change_1h = $(el).find('td:nth-child(5)').text().trim();
-      const change_7d_icon = $(el).find('td:nth-child(7) > span > span');
-      crypto.change_7d_icon = change_7d_icon && change_7d_icon.attr('class') && change_7d_icon.attr('class').indexOf('up') !== -1 ? '✅' : '❗'; // Verifica si el elemento existe
-      crypto.change_7d = $(el).find('td:nth-child(7)').text().trim();
-      cryptos.push(crypto);
-
-      
-
-    });
-    if (cryptos.name != ""){
+        const crypto = {};
+        const name = $(el).find('td:nth-child(3) > div > a > div > div > p').text().trim();
+        if (name) {
+          crypto.name = name;
+          crypto.price = $(el).find('td:nth-child(4) > div > a:first-child').text().trim();
+          const change_1h_icon = $(el).find('td:nth-child(5) > span > span');
+          crypto.change_1h_icon = change_1h_icon && change_1h_icon.attr('class') && change_1h_icon.attr('class').indexOf('up') !== -1 ? '✅' : '❗'; // Verifica si el elemento existe
+          crypto.change_1h = $(el).find('td:nth-child(5)').text().trim();
+          const change_7d_icon = $(el).find('td:nth-child(7) > span > span');
+          crypto.change_7d_icon = change_7d_icon && change_7d_icon.attr('class') && change_7d_icon.attr('class').indexOf('up') !== -1 ? '✅' : '❗'; // Verifica si el elemento existe
+          crypto.change_7d = $(el).find('td:nth-child(7)').text().trim();
+          cryptos.push(crypto);
+        }
+      });
         console.log(cryptos);
-      }
   })
   .catch(console.error);
