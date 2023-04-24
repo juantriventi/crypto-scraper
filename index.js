9dc03750c455826f504cd71f34661f84d42ebca9
juan triventi <jctriventi@ikeasistencia.com.ar>
2023-04-24 15:01:03 -0300

    agrego 10 cripto a la lista con sus valores

---
+++ index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const url = 'https://coinmarketcap.com/';
+axios.get(url)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const cryptos = [];
+    $('tbody tr').each((i, el) => {
+      const crypto = {};
+      crypto.name = $(el).find('td:nth-child(3) > div > a > div > div > p').text().trim();
+      crypto.price = $(el).find('td:nth-child(4) > div > a:first-child').text().trim();
+      const change_1h_icon = $(el).find('td:nth-child(5) > span > span');
+      crypto.change_1h_icon = change_1h_icon && change_1h_icon.attr('class') && change_1h_icon.attr('class').indexOf('up') !== -1 ? '⬆️' : '⬇️'; // Verifica si el elemento existe
+      crypto.change_1h = $(el).find('td:nth-child(5)').text().trim();
+      const change_7d_icon = $(el).find('td:nth-child(7) > span > span');
+      crypto.change_7d_icon = change_7d_icon && change_7d_icon.attr('class') && change_7d_icon.attr('class').indexOf('up') !== -1 ? '⬆️' : '⬇️'; // Verifica si el elemento existe
+      crypto.change_7d = $(el).find('td:nth-child(7)').text().trim();
+      cryptos.push(crypto);
+
+    });
+    console.log(cryptos);
+  })
+  .catch(console.error);
